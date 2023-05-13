9ced248af39ac6fe1e115dfa8a8ff1ac0213b1a6
Ravi <ravkchauhan1aip@gmail.com>
2023-05-13 23:33:53 +0530

    update

--- script.js
+++ script.js
@@ -9,4 +9,9 @@ filter.addEventListener('input', (e) => filterData(e.target.value))
 async function getData() {
     const res = await fetch('https://randomuser.me/api?results=50')
 
+    const { results } = await res.json()
+
+    // Clear result
+    result.innerHTML = ''
+
 }
