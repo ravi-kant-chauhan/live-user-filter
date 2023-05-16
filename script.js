613c76f72f2dbaa4de131463f6bf6e4e9f6ef10d
Ravi <ravkchauhan1aip@gmail.com>
2023-05-16 23:55:18 +0530

    update

--- script.js
+++ script.js
@@ -19,7 +19,13 @@ async function getData() {
 
         listItems.push(li)
 
-        
+        li.innerHTML = `
+            <img src="${user.picture.large}" alt="${user.name.first}">
+            <div class="user-info">
+                <h4>${user.name.first} ${user.name.last}</h4>
+                <p>${user.location.city}, ${user.location.country}</p>
+            </div>
+        `
 
         result.appendChild(li)
     })
